49f9d87376654e1d342224a9a8effea2353fe024
David <david.tejedor@outlook.com>
2019-01-17 13:57:08 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { SpotsComponent } from './spots/spots.component';
+import { ReportsComponent } from './reports/reports.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { EditSpotComponent } from './edit-spot/edit-spot.component';
+import { PublicationsComponent } from './publications/publications.component';
+import { ShowReportComponent } from './show-report/show-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    SpotsComponent,
+    ReportsComponent,
+    EditUserComponent,
+    EditSpotComponent,
+    PublicationsComponent,
+    ShowReportComponent
   ],
   imports: [
     BrowserModule,
